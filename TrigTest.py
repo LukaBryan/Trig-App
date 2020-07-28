48d9a83b55c8cb5080440a0f2da37aba19a18ff5
LukaBryan <18073bry>
2020-07-29 10:50:12 +1200

    More Tkinter testing

--- TrigTest.py
+++ TrigTest.py
@@ -1,14 +1,21 @@
-import tkinter as tk
-from math import*
-from PIL import Image, ImageTk
+from tkinter import *
+from math import *
 
-#Tkinter Things
-window=tk.Tk()
+# Tkinter Things
+window = Tk()
+canvas = Canvas(window, width=1000, height=900)
+img = PhotoImage(file="triangle.gif")
+canvas.create_image(0, -150, anchor=NW, image=img)  # Creates tirangle Graphic
+Hyp = Entry(fg="Black", bg="#FFFFFF", width="20")
+Hyp.pack()
+Hyp.place(x=500, y=400)
+canvas.pack()
+canvas.configure(bg="#FFFFFF")
 window.mainloop()
 
-#main Program
-Deg=int(input("Input angle: "))
-H=int(input("Input Length: "))
-Rad=radians(Deg)
-O=H*sin(Rad)
+# main Program
+Deg = int(input("Input angle: "))
+H = int(input("Input Length: "))
+Rad = radians(Deg)
+O = H * sin(Rad)
 print(O)
