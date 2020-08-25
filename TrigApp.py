d6d8c855ef1f8f38eebf8d54aabfb9677986ab35
LukaBryan <18073bry>
2020-08-26 10:59:19 +1200

    more added to the user interface eg labels and entry boxes

--- TrigApp.py
+++ TrigApp.py
@@ -16,15 +16,60 @@ img = PhotoImage(file="kinghom.gif")  # Import image
 canvas.create_image(500, 500, image=img)  # Creates triangle Graphic
 window.resizable(False, False)  # Stop the ability for the window to be resized
 
+LabelRelXFloat = 0.58
+EntryRelXFloat = 0.8
+Height = 0.08
+HeightInc = 0.06
 
-Hyp = Entry(window, fg="Black", bg="#FFFFFF", width="10", font=("arial", 20))  # Entry box for the H length on triangle
-HypLabel = Label(window, text="H = ", bg="white", font=("arial", 24))
-Hyp.place(relx=0.8, rely=0.08, anchor=CENTER)
-HypLabel.place(relx=0.6, rely=0.08, anchor=CENTER)
 
-Opp = Entry(window, fg="Black", bg="#FFFFFF", width="10", font=("arial", 20))
 
-Adj = Entry(window, fg="Black", bg="#FFFFFF", width="10", font=("arial", 20))
+
+### USER INTERFACE FEATURES
+# Title
+TitleLabel = Label(window, text="Trig Calculator",  font=("arial", 48))
+TitleLabel.place(x=20, y=16, anchor=NW)
+
+# Entries and entry labels
+Hyp = Entry(window, fg="Black",  width="10", font=("arial", 28))  # Entry box for the H length on triangle
+HypLabel = Label(window, text="H = ", bg="white", font=("arial", 32))
+Hyp.place(relx=EntryRelXFloat, rely=Height, anchor=CENTER)
+HypLabel.place(relx=LabelRelXFloat, rely=Height, anchor=CENTER)
+
+Opp = Entry(window, fg="Black", width="10", font=("arial", 28))
+OppLabel = Label(window, text="O = ", bg="white", font=("arial", 32))
+Opp.place(relx=EntryRelXFloat, rely=Height+HeightInc, anchor=CENTER)
+OppLabel.place(relx=LabelRelXFloat, rely=Height+HeightInc, anchor=CENTER)
+
+Adj = Entry(window, fg="Black", width="10", font=("arial", 28))
+AdjLabel = Label(window, text="A = ", bg="white", font=("arial", 32))
+Adj.place(relx=EntryRelXFloat, rely=Height+2*HeightInc, anchor=CENTER)
+AdjLabel.place(relx=LabelRelXFloat, rely=Height+2*HeightInc, anchor=CENTER)
+
+Angle1 = Entry(window, fg="Black", width="10", font=("arial", 28))
+Angle1Label = Label(window, text="∠1 = ", bg="white", font=("arial", 32))
+Angle1.place(relx=EntryRelXFloat, rely=Height+3*HeightInc, anchor=CENTER)
+Angle1Label.place(relx=LabelRelXFloat, rely=Height+3*HeightInc, anchor=CENTER)
+
+Angle2 = Entry(window, fg="Black", bg="#FFFFFF", width="10", font=("arial", 28))
+Angle2Label = Label(window, text="∠2 = ", bg="white", font=("arial", 32))
+Angle2.place(relx=EntryRelXFloat, rely=Height+4*HeightInc, anchor=CENTER)
+Angle2Label.place(relx=LabelRelXFloat, rely=Height+4*HeightInc, anchor=CENTER)
+
+# Triangle labels
+HLabel = Label(window, text="H", bg="white", font=("arial", 32))
+HLabel.place(x=450, y=560, anchor=CENTER)
+
+OLabel = Label(window, text="O", bg="white", font=("arial", 32))
+OLabel.place(x=40, y=620, anchor=CENTER)
+
+ALabel = Label(window, text="A", bg="white", font=("arial", 32))
+ALabel.place(x=360, y=950, anchor=CENTER)
+
+OneLabel = Label(window, text="1", bg="white", font=("arial", 32))
+OneLabel.place(x=595, y=845, anchor=CENTER)
+
+TwoLabel = Label(window, text="2", bg="white", font=("arial", 32))
+TwoLabel.place(x=150, y=450, anchor=CENTER)
 
 window.title("Trig App")  # Sets window name
 ###canvas.configure(bg="grey50")
